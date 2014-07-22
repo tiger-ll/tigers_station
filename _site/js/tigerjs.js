1e91c353a32fc7790e94da066186c471d8d0acd0
tiger-ll <lliu6@pratt.edu>
2014-07-22 12:47:57 -0400

    pushBlueNav

--- _site/js/tigerjs.js
+++ _site/js/tigerjs.js
@@ -2,11 +2,17 @@
 $(document).ready(function(){
 $('.eff').mouseenter(function(){$(this).animate({backgroundColor: '#E4D8B8'})
 });
-$('.eff').mouseleave(function(){$(this).animate({backgroundColor: 'transparent'})
+$('.eff').mouseleave(function(){$(this).animate({backgroundColor: 'white'})
  
 });
 
 $('.active').change(function(){alert($(".active").attr("data-slide-to"));
+});
+
+$('.navbar-toggle').mouseenter(function(){$('.icon-bar').animate({backgroundColor: '#E4D8B8'})
+});
+
+$('.navbar-toggle').mouseleave(function(){$('.icon-bar').animate({backgroundColor: 'black'})
 });
 
 });
